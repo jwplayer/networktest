c9e9df549fcf75ae46419713fed035e5e4f86d7b
Matt Wisniewski <mwisniewski@jwplayer.com>
2019-11-07 13:51:15 -0500

    more classifiers

--- setup.py
+++ setup.py
@@ -37,7 +37,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
